59083d3310d1643ef3daa5cbc8d2a32984f54d37
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2024-10-03 14:16:33 +0200

    Ejercicio 9 terminado

--- src/ejercicio9/Ejercicio9.java
+++ src/ejercicio9/Ejercicio9.java
@@ -4,7 +4,9 @@ public class Ejercicio9 {
 	public static int binario(int valorDecimal) {
 		int res = 0;
 		
+		String temp = Integer.toBinaryString(valorDecimal);
 		
+		res = Integer.valueOf(temp);
 		
 		return res;
 	}
--- src/ejercicio9/Ejercicio9Test.java
+++ src/ejercicio9/Ejercicio9Test.java
@@ -7,8 +7,39 @@ import org.junit.jupiter.api.Test;
 class Ejercicio9Test {
 
 	@Test
-	void testBinario() {
-		
+	void test1Binario() {
+		int res = Ejercicio9.binario(1);
+		assertEquals(1, res);
+	}
+	
+	@Test
+	void test2Binario() {
+		int res = Ejercicio9.binario(2);
+		assertEquals(10, res);
+	}
+	
+	@Test
+	void test3Binario() {
+		int res = Ejercicio9.binario(404);
+		assertEquals(110010100, res);
+	}
+	
+	@Test
+	void test4Binario() {
+		int res = Ejercicio9.binario(25);
+		assertEquals(11001, res);
+	}
+	
+	@Test
+	void test5Binario() {
+		int res = Ejercicio9.binario(1000);
+		assertEquals(1111101000, res);
+	}
+	
+	@Test
+	void test6Binario() {
+		int res = Ejercicio9.binario(0);
+		assertEquals(0, res);
 	}
 
 }
